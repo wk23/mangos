f58ff61d0b50304c7d7166d97ae4134f09ad0372
ApoC <apoc@nymfe.net>
2008-12-07 12:24:26 +0100

    Removed unused column points_to_add from arena_team_member. Replaces some time(NULL) expensive call with inexpensive sWorld.GetGameTime(). Signed-off-by: ApoC <apoc@nymfe.net>

--- sql/updates/arena/Char_Arena.sql
+++ sql/updates/arena/Char_Arena.sql
@@ -2,7 +2,5 @@ CREATE TABLE `saved_variables` (
     `NextArenaPointDistributionTime` timestamp NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Variable Saves';
 
-ALTER TABLE `arena_team_member` ADD COLUMN `points_to_add` int(10) UNSIGNED NOT NULL DEFAULT '0';
-
 ALTER TABLE `arena_team_member` ADD COLUMN `personal_rating` int(10) UNSIGNED NOT NULL DEFAULT '0';
 ALTER TABLE `characters` ADD COLUMN `arena_pending_points` int(10) UNSIGNED NOT NULL default '0';
