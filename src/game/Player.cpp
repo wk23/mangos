47fc4013bea0d03c062b56f4ef3f8cf7b1c8afb6
balrok <balrok@getmangos.com>
2009-12-05 10:19:26 +0000

    [performance-information] logtimer disabled for playersave
    playersaves are done async so no need for logging the time.. it's
    very low..

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -16055,7 +16055,6 @@ bool Player::_LoadHomeBind(QueryResult *result)
 
 void Player::SaveToDB()
 {
-    sWorld.lagLogStart(LAG_LOG_PLAYERSAVE);
     // we should assure this: assert((m_nextSave != sWorld.getConfig(CONFIG_INTERVAL_SAVE)));
     assert(!m_isSaved);
     m_isSaved = true;
@@ -16195,7 +16194,6 @@ void Player::SaveToDB()
     if(Pet* pet = GetPet())
         pet->SavePetToDB(PET_SAVE_AS_CURRENT);
     m_isSaved = false;
-    sWorld.lagLogStop(LAG_LOG_PLAYERSAVE, true);
 }
 
 // fast save function for item/money cheating preventing - save only inventory and money state
